ad04b797f9701888e8bef42577ef86af79eb128f
Vladyslav Atamaniuk <115092059+H1mansh0@users.noreply.github.com>
2023-11-27 22:45:36 +0200

    Update PaymentCardController.java

--- src/main/java/ua/edu/ucu/apps/lab/controllers/PaymentCardController.java
+++ src/main/java/ua/edu/ucu/apps/lab/controllers/PaymentCardController.java
@@ -4,14 +4,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.Getter;
 import ua.edu.ucu.apps.lab.payment.CreditCartPaymentStrategy;
 
 @RestController
 @RequestMapping("/api/payment")
+@Getter
 public class PaymentCardController {
+    private final double priceOf = 18.8;
+
     @GetMapping("/card")
-	public double getPayment() {
-		double price = 18.8;
-		return new CreditCartPaymentStrategy().pay(price);
-	}
+    public double getPayment() {
+        return new CreditCartPaymentStrategy().pay(priceOf);
+    }
 }
