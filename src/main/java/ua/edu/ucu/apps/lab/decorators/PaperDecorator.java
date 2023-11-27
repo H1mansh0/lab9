3702e51f7e6a308e6709415f8a623d09f33eff65
Vladyslav Atamaniuk <115092059+H1mansh0@users.noreply.github.com>
2023-11-27 22:51:34 +0200

    Update PaperDecorator.java

--- src/main/java/ua/edu/ucu/apps/lab/decorators/PaperDecorator.java
+++ src/main/java/ua/edu/ucu/apps/lab/decorators/PaperDecorator.java
@@ -1,15 +1,19 @@
 package ua.edu.ucu.apps.lab.decorators;
 
+import lombok.Getter;
 import ua.edu.ucu.apps.lab.flowers.Item;
 
-public class PaperDecorator extends ItemDecorator{
+@Getter
+public class PaperDecorator extends ItemDecorator {
+    private final int addPrice = 13;
+
     public PaperDecorator(Item item) {
         super(item);
     }
 
     @Override
     public double price() {
-        return super.price() + 13;
+        return super.price() + addPrice;
     }
 
     @Override
