29ea8f2b6ab31dc05e22860c9dece55f1eae2d7f
Vladyslav Atamaniuk <115092059+H1mansh0@users.noreply.github.com>
2023-11-27 22:59:35 +0200

    Update Main.java

--- src/main/java/ua/edu/ucu/apps/lab/Main.java
+++ src/main/java/ua/edu/ucu/apps/lab/Main.java
@@ -8,19 +8,19 @@ import ua.edu.ucu.apps.lab.flowers.FlowerType;
 import ua.edu.ucu.apps.lab.flowers.Item;
 
 public class Main {
-    private final int IDONE = 1;
-    private final int IDTWO = 2;
-    private final FlowerType FLOWERTYPE = FlowerType.ROSE;
-    private final int PRICE = 90;
-    private final double SEPALLENGTH = 0.8;
-    private final FlowerColor COLOR = FlowerColor.RED;
+    private final int idOne = 1;
+    private final int idTwo = 2;
+    private final FlowerType flowerType = FlowerType.ROSE;
+    private final int price = 90;
+    private final double sepalLength = 0.8;
+    private final FlowerColor color = FlowerColor.RED;
     
     public static void main(String[] args) {
-        Item item = new Flower(IDONE, FLOWERTYPE, PRICE, SEPALLENGTH, COLOR);
+        Item item = new Flower(idOne, flowerType, price, sepalLength, color);
         item = new DiscountDecorator(new PaperDecorator(item));
         System.out.println(item.price());
 
-        Item itemTwo = new Flower(IDTWO, FLOWERTYPE, PRICE, SEPALLENGTH, COLOR);
+        Item itemTwo = new Flower(idTwo, flowerType, price, sepalLength, color);
         item2 = new PaperDecorator(new DiscountDecorator(itemTwo));
         System.out.println(itemTwo.price());
     }
