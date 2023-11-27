ce026023f0b3463045d620df27e4ebbab71fa6ae
Vladyslav Atamaniuk <115092059+H1mansh0@users.noreply.github.com>
2023-11-27 22:59:58 +0200

    Update DiscountDecorator.java

--- src/main/java/ua/edu/ucu/apps/lab/decorators/DiscountDecorator.java
+++ src/main/java/ua/edu/ucu/apps/lab/decorators/DiscountDecorator.java
@@ -3,13 +3,13 @@ package ua.edu.ucu.apps.lab.decorators;
 import ua.edu.ucu.apps.lab.flowers.Item;
 
 public class DiscountDecorator extends ItemDecorator {
-    private final double DISCOUNT = 0.8;
+    private final double discount = 0.8;
     
     public DiscountDecorator(Item item) {
         super(item);
     }
 
     public double price() {
-        return super.price() * DISCOUNT;
+        return super.price() * discount;
     }
 }
