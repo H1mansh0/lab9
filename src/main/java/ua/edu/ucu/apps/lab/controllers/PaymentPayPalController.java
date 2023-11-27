c065ef6251f4c59f1fa7155bc1cfca3cd517e771
Vladyslav Atamaniuk <115092059+H1mansh0@users.noreply.github.com>
2023-11-27 22:46:11 +0200

    Update PaymentPayPalController.java

--- src/main/java/ua/edu/ucu/apps/lab/controllers/PaymentPayPalController.java
+++ src/main/java/ua/edu/ucu/apps/lab/controllers/PaymentPayPalController.java
@@ -4,14 +4,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.Getter;
 import ua.edu.ucu.apps.lab.payment.PayPalPaymentStrategy;
 
 @RestController
 @RequestMapping("/api/payment")
+@Getter
 public class PaymentPayPalController {
+    private final double priceOf = 182.8;
+
     @GetMapping("/paypal")
-	public double getPayment() {
-		double price = 182.8;
-		return new PayPalPaymentStrategy().pay(price);
-	}
+    public double getPayment() {
+        return new PayPalPaymentStrategy().pay(priceOf);
+    }
 }
