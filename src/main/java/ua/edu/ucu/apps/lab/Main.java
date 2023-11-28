67539cf20f89d02d4c4228cff61326bbdd050a9d
Vladyslav Atamaniuk <115092059+H1mansh0@users.noreply.github.com>
2023-11-28 17:04:39 +0200

    Update Main.java

--- src/main/java/ua/edu/ucu/apps/lab/Main.java
+++ src/main/java/ua/edu/ucu/apps/lab/Main.java
@@ -8,19 +8,19 @@ import ua.edu.ucu.apps.lab.flowers.FlowerType;
 import ua.edu.ucu.apps.lab.flowers.Item;
 
 public class Main {
-    private static final int idOne = 1;
-    private static final int idTwo = 2;
-    private static final FlowerType flowerType = FlowerType.ROSE;
-    private static final int price = 90;
-    private static final double sepalLength = 0.8;
-    private static final FlowerColor color = FlowerColor.RED;
+    private static final int IDONE = 1;
+    private static final int IDTWO = 2;
+    private static final FlowerType FLOWERTYPE = FlowerType.ROSE;
+    private static final int PRICE = 90;
+    private static final double SEPALLENGTH = 0.8;
+    private static final FlowerColor COLOR = FlowerColor.RED;
     
     public static void main(String[] args) {
-        Item item = new Flower(idOne, flowerType, price, sepalLength, color);
+        Item item = new Flower(IDONE, FLOWERTYPE, PRICE, SEPALLENGTH, COLOR);
         item = new DiscountDecorator(new PaperDecorator(item));
         System.out.println(item.price());
 
-        Item itemTwo = new Flower(idTwo, flowerType, price, sepalLength, color);
+        Item itemTwo = new Flower(IDTWO, FLOWERTYPE, PRICE, SEPALLENGTH, COLOR);
         itemTwo = new PaperDecorator(new DiscountDecorator(itemTwo));
         System.out.println(itemTwo.price());
     }
